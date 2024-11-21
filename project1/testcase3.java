8b40d246db84f54d4ab3d52a3038abe25399fee7
BHAVANNI V <42145070+bhavanni@users.noreply.github.com>
2024-11-21 22:49:25 +0530

    Bhavanni add with emailable and index report

---
+++ project1/testcase3.java
@@ -0,0 +1,51 @@
+package project1;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class testcase3 {
+	@Test
+	public  void runloginout() throws InterruptedException {
+		//Launch the browser.
+
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		
+		//Load the URL
+		driver.get("https://dev.valarhr.com/");
+		
+		//Enter the username
+		WebElement fn = driver.findElement(By.xpath("//input[@formcontrolname='username']"));
+		fn.sendKeys("riyaz@rabiyat.fusion");
+		
+		//Enter the password
+		WebElement psw = driver.findElement(By.xpath("//input[@formcontrolname='password']"));
+		psw.sendKeys("fusion123");
+		
+		//Click on the Login button
+		WebElement login = driver.findElement(By.xpath("//button[@class='login-button']"));
+		login.click();
+		
+		//Print the Title
+		System.out.println("Title Name : "+driver.getTitle());
+		
+		
+		//Click on the Logout button
+		WebElement b1 = driver.findElement(By.xpath("//div[@class='hover-base ng-star-inserted']//p[1]"));
+		Actions opt = new Actions(driver);
+		opt.moveToElement(b1).perform();
+		
+		WebElement logout = driver.findElement(By.xpath("//span[text()='Log Out']"));
+		logout.click();
+		
+		driver.quit();
+			
+}
+
+}
